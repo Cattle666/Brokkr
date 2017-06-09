506edccbd02aeca2e87a6d8b792758b6ebfa80e6
Miles Lowroad <akthunderfolk@gmail.com>
2017-06-09 13:20:23 -0600

    fixed some indentations

--- brokkr.py
+++ brokkr.py
@@ -21,14 +21,14 @@ os.chdir(path.dirname(path.abspath(__file__)))
 brokkr = Bot(command_prefix="$")
 
 async def joinVoiceChannel():
-'''Example of simple script for bot to join specific voice channel'''    
+    '''Example of simple script for bot to join specific voice channel'''    
     channel = brokkr.get_channel('300631190002466818')
     voice = await brokkr.join_voice_channel(channel)
     print('Brokkr has joined Staff')
 
 @brokkr.event
 async def on_ready():
-'''Completion of joining voice channel and backend identification...'''
+    '''Completion of joining voice channel and backend identification...'''
     print('Logged in as:')
     print('Username: ' + brokkr.user.name)
     print('ID: ' + brokkr.user.id)
@@ -40,7 +40,7 @@ async def on_ready():
 @brokkr.event
 async def make_trigger(word):
     return r"(^| |[" + string.punctuation + "])" + word + r"( |[" + string.punctuation + "]|$)"
-'''Work in progress. Call and response fun with specific terms.'''
+    '''Work in progress. Call and response fun with specific terms.'''
 @brokkr.event
 async def cookie(self, message, match):
     await make_trigger("cookie")
@@ -63,14 +63,14 @@ async def shit(self, message, match):
 
 @brokkr.event
 async def on_message(message):
-'''Archaic help message. Could use some revision.'''
+    '''Archaic help message. Could use some revision.'''
     if message.content.startswith('$help'):
         helpmsg = 'Greetings {0.author.mention} , here are the commands:\n```$test``` = shows the amount of messages user has in a channel\n ```$sleep``` = puts the bot to sleep momentarily\n ```$hello``` = bot says hello to user\n ```$repo_wad``` = lists wads in VGP repository\n ```$repo_pk3``` = lists pk3s in the VGP repository\n ```$quit``` = Logs Brokkr out of the server\n ```$meow``` = posts a random rare cat pic\n ```$search``` = Search VGP repo for a game file'.format(message)
         await brokkr.send_message(message.channel, helpmsg)
 
-'''Pulled from discord.py example bot.'''
     if message.content.startswith('$test'):
-        counter = 0
+        '''Pulled from discord.py example bot.'''
+    	counter = 0
         tmp = await brokkr.send_message(message.channel, 'Calculating messages...')
         async for log in brokkr.logs_from(message.channel, limit=100):
             if log.author == message.author:
@@ -90,28 +90,28 @@ async def on_message(message):
         await brokkr.send_message(message.channel, msg) 
 
     if message.content.startswith('$repo_wad'):
-'''Displays all .wad files contained in the VGP repository'''
+        '''Displays all .wad files contained in the VGP repository'''
         for file in os.listdir("/srv/zandronum/wads/"):
             if file.endswith(".wad"):
                 wads = os.path.join(file).format(message)
                 await brokkr.send_message(message.channel, wads)    
     
     if message.content.startswith('$repo_pk3'):
-'''Displays all .pk3 files contained in the VGP repository'''
+        '''Displays all .pk3 files contained in the VGP repository'''
         for file in os.listdir("/srv/zandronum/wads/"):
             if file.endswith(".pk3"):
                 pk3s = os.path.join(file).format(message)
                 await brokkr.send_message(message.channel, pk3s)
 
     if message.content.startswith('$meow'):
-'''Random cat. Pulled from discord.py example'''
+        '''Random cat. Pulled from discord.py example'''
         async with aiohttp.get('http://random.cat/meow') as r:
             if r.status == 200:
                 js = await r.json()
                 await brokkr.send_message(message.channel, js['file'])
 
     if message.content.startswith('$search'):
-'''Search VGP repository for specific game file.'''
+        '''Search VGP repository for specific game file.'''
         await brokkr.send_message(message.channel, 'Type $file [filename]')
 
         def check(msg):
